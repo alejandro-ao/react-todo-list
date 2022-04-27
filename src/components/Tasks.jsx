0d5e876bc3e40bf8caa7ae95fb68d45f876d5dd2
Alejandro <aavila0707@gmail.com>
2022-04-27 20:59:47 +0200

    feat: map tasks from js array

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react";
 import Task from "./Task";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      name: "call friend",
+      checked: false,
+    },
+    {
+      id: 2,
+      name: "call mother",
+      checked: true,
+    },
+    {
+      id: 3,
+      name: "call brother",
+      checked: true,
+    },
+    {
+      id: 4,
+      name: "call friend",
+      checked: false,
+    },
+    {
+      id: 5,
+      name: "call mother",
+      checked: true,
+    },
+    {
+      id: 6,
+      name: "call brother",
+      checked: true,
+    },
+  ]);
+
   return (
     <div className="todo-list__tasks mt-3">
-      <Task taskName={"Do Laundry"} checked={false} />
-      <Task taskName={"Do Laundry"} checked={true} />
-      <Task taskName={"Do Laundry"} checked={false} />
-      <Task taskName={"Do Laundry"} checked={true} />
+      {tasks.map((task) => (
+        <Task key={task.id} taskName={task.name} checked={task.checked} />
+      ))}
     </div>
   );
 };
