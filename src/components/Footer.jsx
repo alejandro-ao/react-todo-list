e5f047e5e90fbf74f4df406f33c4000118154c2d
Alejandro <aavila0707@gmail.com>
2022-04-27 20:17:52 +0200

    feat: prepare layout w bootstrap

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="todo-list__footer mt-5">Thank you for using Hanabi Todo List</div>;
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="todo-list__header">
+      <h1>Hanabi Todo List</h1>
+    </div>
+  );
+};
+
+export default Header;
